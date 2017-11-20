376554edb248bda50946395bd8c0c8937ca5b889
Marcell Bán <afga93+cc@gmail.com>
2017-11-20 13:34:33 +0100

    switch to dictionary style

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -8,42 +8,42 @@ namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Beat yo' ass #1.9.1";
+		public static readonly string VERSION = "Beat yo' ass #1.9.2";
 
 		public static int BetRequest(JObject gameState)
 		{
             try
             {
 
-                int playerId = gameState.GetValue("in_action").ToObject<int>();
+                int playerId = gameState["in_action"].ToObject<int>();
                 Console.Error.WriteLine("playerId ok");
-                List<JToken> players = gameState.GetValue("players").Values().ToList();
+                List<JToken> players = gameState["players"].Values().ToList();
                 Console.Error.WriteLine("players ok");
 
-                int currentBuyIn = gameState.GetValue("current_buy_in").ToObject<int>();
+                int currentBuyIn = gameState["current_buy_in"].ToObject<int>();
                 Console.Error.WriteLine("currentBuyIn ok");
-                int minRaise = gameState.GetValue("minimum_raise").ToObject<int>();
+                int minRaise = gameState["minimum_raise"].ToObject<int>();
                 Console.Error.WriteLine("minRaise ok");
 
-                JObject ourPlayer = players[playerId].Value<JObject>();
+                JToken ourPlayer = players[playerId];
                 Console.Error.WriteLine("ourPlayer ok");
 
-                var ourHand = ourPlayer.GetValue("hole_cards").Values().ToList();
-                var firstCardRank = ourHand[0].ToObject<JObject>().GetValue("rank").ToObject<string>();
-                var secondCardRank = ourHand[1].ToObject<JObject>().GetValue("rank").ToObject<string>();
+                var ourHand = ourPlayer["hole_cards"].Values().ToList();
+                var firstCardRank = ourHand[0]["rank"].ToObject<string>();
+                var secondCardRank = ourHand[1]["rank"].ToObject<string>();
 
                 Console.Error.WriteLine(firstCardRank);
                 Console.Error.WriteLine(secondCardRank);
 
                 if (firstCardRank.Equals(secondCardRank))
                 {
-                    return currentBuyIn - ourPlayer.GetValue("bet").ToObject<int>() + minRaise;
+                    return currentBuyIn - ourPlayer["bet"].ToObject<int>() + minRaise;
                 }
 
                 string[] highCards = new string[4] { "J", "K", "Q", "A" };
                 if (highCards.Contains(firstCardRank) || highCards.Contains(secondCardRank))
                 {
-                    return currentBuyIn - ourPlayer.GetValue("bet").ToObject<int>() + minRaise;
+                    return currentBuyIn - ourPlayer["bet"].ToObject<int>() + minRaise;
                 }
 
             }
