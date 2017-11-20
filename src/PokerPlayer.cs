a414b06069e853d1f9344075b6c08040ac37d7bb
Marcell Bán <afga93+cc@gmail.com>
2017-11-20 13:43:33 +0100

    ourhand fix

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -8,7 +8,7 @@ namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Beat yo' ass #1.9.3";
+		public static readonly string VERSION = "Beat yo' ass #1.9.4";
 
 		public static int BetRequest(JObject gameState)
 		{
@@ -28,7 +28,7 @@ namespace Nancy.Simple
                 JToken ourPlayer = players[playerId];
                 Console.Error.WriteLine("ourPlayer ok");
 
-                var ourHand = ourPlayer["hole_cards"].Values().ToList();
+                var ourHand = ourPlayer["hole_cards"].ToList();
                 Console.Error.WriteLine("ourHand ok");
                 var firstCardRank = ourHand[0]["rank"].ToObject<string>();
                 Console.Error.WriteLine("firstcard ok");
