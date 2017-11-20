6e6d86530b191bdb40da28cff150721fe392e690
peter.osz <itharius@gmail.com>
2017-11-20 11:26:59 +0100

    stacktrace logging

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -1,38 +1,48 @@
 ﻿using Newtonsoft.Json.Linq;
 using System.Linq;
 using System.Collections;
+using System;
 
 namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Beat yo' ass #1.1";
+		public static readonly string VERSION = "Beat yo' ass #1.2";
 
 		public static int BetRequest(JObject gameState)
 		{
-            int playerId = gameState.GetValue("in_action").Value<int>();
-            var players = gameState.GetValue("players").Values().ToList();
-
-            int currentBuyIn = gameState.GetValue("current_buy_in").Value<int>();
-            int minRaise = gameState.GetValue("minumum_raise").Value<int>();
-
-            JObject ourPlayer = players[playerId].Value<JObject>();
-
-            var ourHand = ourPlayer.GetValue("hole_cards").Values().ToList();
-            var firstCardRank = ourHand[0].Value<JObject>().GetValue("rank").Value<string>();
-            var secondCardRank = ourHand[1].Value<JObject>().GetValue("rank").Value<string>();
-
-            if (firstCardRank.Equals(secondCardRank))
+            try
             {
-                return currentBuyIn - ourPlayer.GetValue("bet").Value<int>() + minRaise;
-            }
 
-            string[] highCards = new string[4] { "J", "K", "Q", "A" };
-            if (highCards.Contains(firstCardRank) || highCards.Contains(secondCardRank))
+                int playerId = gameState.GetValue("in_action").Value<int>();
+                var players = gameState.GetValue("players").Values().ToList();
+
+                int currentBuyIn = gameState.GetValue("current_buy_in").Value<int>();
+                int minRaise = gameState.GetValue("minumum_raise").Value<int>();
+
+                JObject ourPlayer = players[playerId].Value<JObject>();
+
+                var ourHand = ourPlayer.GetValue("hole_cards").Values().ToList();
+                var firstCardRank = ourHand[0].Value<JObject>().GetValue("rank").Value<string>();
+                var secondCardRank = ourHand[1].Value<JObject>().GetValue("rank").Value<string>();
+
+                if (firstCardRank.Equals(secondCardRank))
+                {
+                    return currentBuyIn - ourPlayer.GetValue("bet").Value<int>() + minRaise;
+                }
+
+                string[] highCards = new string[4] { "J", "K", "Q", "A" };
+                if (highCards.Contains(firstCardRank) || highCards.Contains(secondCardRank))
+                {
+                    return currentBuyIn - ourPlayer.GetValue("bet").Value<int>() + minRaise;
+                }
+
+            }
+            catch(Exception ex)
             {
-                return currentBuyIn - ourPlayer.GetValue("bet").Value<int>() + minRaise;
+                Console.Error.WriteLine(ex.StackTrace);
             }
-
+        
             return 0;
 		}
 
