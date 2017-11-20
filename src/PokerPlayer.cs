acc863dcee7c9a2b1900879282c9f17e1e7dfa27
peter.osz <itharius@gmail.com>
2017-11-20 10:44:32 +0100

    when pair in hand, raise

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -1,15 +1,33 @@
 ﻿using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Collections;
 
 namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Default C# folding player";
+		public static readonly string VERSION = "Beat yo' ass #1.0";
 
 		public static int BetRequest(JObject gameState)
 		{
-			//TODO: Use this method to return the value You want to bet
-			return 0;
+            int playerId = gameState.GetValue("in_action").Value<int>();
+            var players = gameState.GetValue("players").Values().ToList();
+
+            int currentBuyIn = gameState.GetValue("current_buy_in").Value<int>();
+            int minRaise = gameState.GetValue("minumum_raise").Value<int>();
+
+            JObject ourPlayer = players[playerId].Value<JObject>();
+
+            var ourHand = ourPlayer.GetValue("hole_cards").Values().ToList();
+            var firstCardRank = ourHand[0].Value<JObject>().GetValue("rank").Value<string>();
+            var secondCardRank = ourHand[1].Value<JObject>().GetValue("rank").Value<string>();
+
+            if (firstCardRank.Equals(secondCardRank))
+            {
+                return currentBuyIn - ourPlayer.GetValue("bet").Value<int>() + minRaise;
+            }
+
+            return 0;
 		}
 
 		public static void ShowDown(JObject gameState)
