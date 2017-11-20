64e3fc2dc3b89d0555866cf479388225fd5ef084
peter.osz <itharius@gmail.com>
2017-11-20 11:38:51 +0100

    changed to int parsing

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -7,18 +7,18 @@ namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Beat yo' ass #1.2";
+		public static readonly string VERSION = "Beat yo' ass #1.3";
 
 		public static int BetRequest(JObject gameState)
 		{
             try
             {
 
-                int playerId = gameState.GetValue("in_action").Value<int>();
+                int playerId = Int32.Parse(gameState.GetValue("in_action").Value<string>());
                 var players = gameState.GetValue("players").Values().ToList();
 
-                int currentBuyIn = gameState.GetValue("current_buy_in").Value<int>();
-                int minRaise = gameState.GetValue("minumum_raise").Value<int>();
+                int currentBuyIn = Int32.Parse(gameState.GetValue("current_buy_in").Value<string>());
+                int minRaise = Int32.Parse(gameState.GetValue("minumum_raise").Value<string>());
 
                 JObject ourPlayer = players[playerId].Value<JObject>();
 
@@ -28,13 +28,13 @@ namespace Nancy.Simple
 
                 if (firstCardRank.Equals(secondCardRank))
                 {
-                    return currentBuyIn - ourPlayer.GetValue("bet").Value<int>() + minRaise;
+                    return currentBuyIn - Int32.Parse(ourPlayer.GetValue("bet").Value<string>()) + minRaise;
                 }
 
                 string[] highCards = new string[4] { "J", "K", "Q", "A" };
                 if (highCards.Contains(firstCardRank) || highCards.Contains(secondCardRank))
                 {
-                    return currentBuyIn - ourPlayer.GetValue("bet").Value<int>() + minRaise;
+                    return currentBuyIn - Int32.Parse(ourPlayer.GetValue("bet").Value<string>()) + minRaise;
                 }
 
             }
